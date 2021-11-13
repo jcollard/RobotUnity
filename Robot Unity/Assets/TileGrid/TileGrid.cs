93fb632026472d00e12dbf16c350afeb44ea76ed
Joseph Collard <jcollard@users.noreply.github.com>
2021-11-13 11:01:50 -0800

    feat: Add loaded indicator in Inspector. Added a message in the inspector window for TileGrid to display a message to users if the TileGrid was loaded or not loaded. If the TileGrid could not be loaded and an Exception was thrown during GenerateGrid() the Exception's message is displayed in the inspector.

--- Robot Unity/Assets/TileGrid/TileGrid.cs
+++ Robot Unity/Assets/TileGrid/TileGrid.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEditor;
-using System.IO;
 using Collard;
 
 public class TileGrid : MonoBehaviour
@@ -11,11 +10,21 @@ public class TileGrid : MonoBehaviour
 
     private TextAsset mapFile;
     private TileFactory factory;
-    private bool isDirty = true;
+    private bool _IsDirty = true;
+    private bool IsDirty
+    {
+        get => ErrorMessage != null || _IsDirty;
+        set => _IsDirty = value;
+    }
     private GameObject[,] grid;
+    public bool IsLoaded
+    {
+        get => this.IsDirty == false && this.grid != null;
+    }
+    public string ErrorMessage { get; private set; }
 
-    private int rows = 1;
-    private int columns = 1;
+    public int Rows { get => this.grid != null ? this.grid.GetLength(0) : -1; }
+    public int Columns { get => this.grid != null ? this.grid.GetLength(1) : -1; }
 
     public TextAsset MapFile
     {
@@ -23,59 +32,88 @@ public class TileGrid : MonoBehaviour
         {
             if (this.mapFile == null)
             {
-                this.mapFile = new TextAsset("1x1\n#");
+                this.mapFile = new TextAsset("1x1\n.");
             }
             return this.mapFile;
         }
-        set => this.mapFile = value;
+
+        set
+        {
+            if (this.mapFile == value)
+            {
+                return;
+            }
+
+            this.mapFile = value ?? throw new ArgumentNullException("Cannot specify a null Map File.");
+            this.IsDirty = true;
+        }
     }
 
     public TileFactory Factory
     {
         get => this.factory;
-        set => this.factory = value ?? throw new ArgumentNullException("Factory must be non-null.");
+        set
+        {
+            if (this.factory == value)
+            {
+                return;
+            }
+
+            this.factory = value ?? throw new ArgumentNullException("Factory must be non-null.");
+            this.IsDirty = true;
+        }
     }
 
     public void CleanUp()
     {
-        if (!this.isDirty)
+        if (!this.IsDirty)
         {
             return;
         }
 
+        this.IsDirty = false;
         this.GenerateGrid();
-        this.isDirty = false;
     }
 
     public void GenerateGrid()
     {
-        UnityUtils.DeleteChildren(this.transform);
+        UnityUtils.DestroyImmediateChildren(this.transform);
         List<String> lines = Utils.GetStringIterable(mapFile.text).ToList();
-        char[,] grid = MapFileParser.Instance.Parse(lines);
-        this.rows = grid.GetLength(0);
-        this.columns = grid.GetLength(1);
-        this.grid = new GameObject[rows, columns];
-
-        float offsetX = -((float)(this.rows - 1)) * 0.5f;
-        float offsetZ = -((float)(this.columns - 1)) * 0.5f;
-
-        foreach ((int row, int col, char c) in Utils.Get2DEnumerable(grid))
+        this.grid = null;
+        try
         {
-            GameObject newTile;
-            if (this.factory == null)
+            char[,] grid = MapFileParser.Instance.Parse(lines);
+            this.grid = new GameObject[grid.GetLength(0), grid.GetLength(1)];
+            float offsetX = -((float)(this.Rows - 1)) * 0.5f;
+            float offsetZ = -((float)(this.Columns - 1)) * 0.5f;
+
+            foreach ((int row, int col, char c) in Utils.Get2DEnumerable(grid))
             {
-                newTile = TileGrid.GetDefaultTile();
+                GameObject newTile;
+                if (this.factory == null)
+                {
+                    newTile = TileGrid.GetDefaultTile();
+                }
+                else if (!this.factory.IsValidTile(c))
+                {
+                    newTile = this.factory.GetTile(c);
+                }
+                else
+                {
+                    throw new InvalidTileCharacterException($"An invalid tile character {c} was found at {row}x{col}.");
+                }
+                newTile.transform.parent = this.transform;
+                newTile.transform.localPosition = new Vector3(row + offsetX, 0, col + offsetZ);
+                this.grid[row, col] = newTile;
             }
-            else if (!this.factory.IsValidTile(c))
-            {
-                newTile = this.factory.GetTile(c);
-            }
-            else
-            {
-                throw new InvalidTileCharacterException($"An invalid tile character {c} was found at {row}x{col}.");
-            }
-            newTile.transform.parent = this.transform;
-            this.transform.localPosition = new Vector3(row + offsetX, 0, col + offsetZ);
+
+            this.ErrorMessage = null;
+            this.IsDirty = false;
+        }
+        catch (Exception e)
+        {
+            this.ErrorMessage = e.Message;
+            throw e;
         }
     }
 
@@ -91,6 +129,7 @@ public class TileGrid : MonoBehaviour
 [CustomEditor(typeof(TileGrid))]
 public class TileGridEditor : Editor
 {
+
     public override void OnInspectorGUI()
     {
         TileGrid tileGrid = (TileGrid)target;
@@ -98,19 +137,32 @@ public class TileGridEditor : Editor
         tileGrid.MapFile = (TextAsset)EditorGUILayout.ObjectField("Map File", tileGrid.MapFile, typeof(TextAsset), false);
         EditorGUILayout.ObjectField("Tile Factory", tileGrid.Factory, typeof(MapFileParser), true);
 
+        GUILayout.Space(20);
+        if (tileGrid.IsLoaded)
+        {
+
+            EditorGUILayout.LabelField("Tile Grid Loaded", UnityUtils.GetColorLabel(Color.green));
+            EditorGUILayout.LabelField($"Dimensions: {tileGrid.Rows}x{tileGrid.Columns}");
+        }
+        else
+        {
+            EditorGUILayout.LabelField("Tile Grid Not Loaded", UnityUtils.GetColorLabel(Color.red));
+        }
+
+        if (!tileGrid.IsLoaded && tileGrid.ErrorMessage != null)
+        {
+            GUILayout.Space(20);
+            EditorGUILayout.HelpBox(tileGrid.ErrorMessage, MessageType.Error);
+        }
+
 
         GUILayout.Space(20);
         if (GUILayout.Button("Rebuild Tile Grid"))
         {
-            //tileGrid.CleanUp();
             tileGrid.GenerateGrid();
         }
 
-
-        //EditorGUILayout.HelpBox("The rows and columns must be >0.", MessageType.Info);
-
-        //tileGrid.CleanUp();
-
+        tileGrid.CleanUp();
     }
 }
 
