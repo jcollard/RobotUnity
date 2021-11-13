437038f8a8e5fab764aea048478d54e4357dcfc0
Joseph Collard <jcollard@users.noreply.github.com>
2021-11-13 10:58:58 -0800

    feat: Update UnityUtils Renamed `DeleteChildren` -> `DestroyImmediateChildren` to match the `DestroyImmediate` method in UnityEngine.Object.
    Added `GetColorLabel` for quick colored GUILabels.

--- Robot Unity/Assets/Collard/UnityUtils.cs
+++ Robot Unity/Assets/Collard/UnityUtils.cs
@@ -1,12 +1,27 @@
 namespace Collard
 {
     using UnityEngine;
+    using UnityEditor;
     using System.Collections.Generic;
     using System.Linq;
+    using System;
 
     public class UnityUtils
     {
-        public static void DeleteChildren(Transform t)
+        private static readonly Dictionary<Color, GUIStyle> ColorLabels = new Dictionary<Color, GUIStyle>();
+
+        public static GUIStyle GetColorLabel(Color color)
+        {
+            if (!ColorLabels.TryGetValue(color, out GUIStyle style))
+            {
+                style = new GUIStyle(EditorStyles.label);
+                style.normal.textColor = color;
+            }
+
+            return style;
+        }
+
+        public static void DestroyImmediateChildren(Transform t)
         {
             List<Transform> children = t.Cast<Transform>().ToList();
             foreach (Transform child in children)
